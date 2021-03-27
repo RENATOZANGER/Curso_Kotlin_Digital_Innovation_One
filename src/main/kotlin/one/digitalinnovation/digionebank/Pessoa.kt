c5d916678a8da7364a7b2769c562b935f08d1941
RenatoZangerolami <renato.zanger@gmail.com>
2021-03-27 15:35:26 -0300

    Autalizado para tornar Pessoa e Funcionario uma classe abstrata.

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Renato"
-    var cpf: String = "111.111.111-11"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main(){
-    val renato = Pessoa()
-    println(renato.pessoaInfo())
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
