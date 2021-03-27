9d0156b1f5cab1aa0ec9bc58060a514a7c5d7021
RenatoZangerolami <renato.zanger@gmail.com>
2021-03-27 13:08:47 -0300

    Atualização para aplicar o modificador privado para alterar a função da propriedade do cpf

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,13 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Renato"
     var cpf: String = "111.111.111-11"
+    private set
 }
 
 fun main(){
     val renato = Pessoa()
+
+    println(renato)
     println(renato.nome)
     println(renato.cpf)
 
